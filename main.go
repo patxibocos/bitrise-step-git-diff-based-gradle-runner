06c2ce1ac60b23b9bb5e8d4585cd43f6a0818774
Patxi Bocos <patxi952@gmail.com>
2020-08-21 17:58:19 +0200

    Bitrise step skeleton

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ type gradleProject struct {
 }
 
 func main() {
+	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
 	projectDir := "C:/Users/oopat/Desktop/MyApplication"
 	changedFiles, err := getChangedFilesBetweenBranches(projectDir, "master", "test")
 	if err != nil {
@@ -31,6 +32,26 @@ func main() {
 		fmt.Println(err)
 		panic("Exiting... could not get Gradle subprojects")
 	}
+
+	//
+	// --- Step Outputs: Export Environment Variables for other Steps:
+	// You can export Environment Variables for other Steps with
+	//  envman, which is automatically installed by `bitrise setup`.
+	// A very simple example:
+	//cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+	//if err != nil {
+	//	fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
+	//	os.Exit(1)
+	//}
+	// You can find more usage examples on envman's GitHub page
+	//  at: https://github.com/bitrise-io/envman
+
+	//
+	// --- Exit codes:
+	// The exit code of your Step is very important. If you return
+	//  with a 0 exit code `bitrise` will register your Step as "successful".
+	// Any non zero exit code will be registered as "failed" by `bitrise`.
+	os.Exit(0)
 }
 
 func getChangedFilesBetweenBranches(baseDir string, baseBranch string, targetBranch string) ([]string, error) {
